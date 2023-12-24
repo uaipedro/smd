43752a093f26a6602ed3e2d9f2633d2bd6c9d38f
uaipedro <pedromambelli@gmail.com>
2023-12-24 11:39:54 -0300

    Add progress bar to specie loading process

--- smd/load_specie.py
+++ smd/load_specie.py
@@ -1,6 +1,7 @@
 import time
 
 import typer
+from rich.progress import Progress, SpinnerColumn, TextColumn
 from typing_extensions import Annotated
 
 from smd.bioclim.bioclim_service import BioclimService
@@ -23,24 +24,34 @@ def load_specie(
 ):
     typer.echo(f"Loading specie from gbif: {specie}")
     now = time.time()
-    occurrences = busca_especie_no_gbif(specie)
 
-    occurrences = BioclimService().augment_data_with_bioclimate(occurrences)
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        task_id = progress.add_task("Loading specie from gbif", total=None)
+        occurrences = busca_especie_no_gbif(specie)
+        progress.remove_task(task_id)
 
-    if output == "output":
-        output = specie.replace(" ", "_")
+        task_id = progress.add_task("Augmenting data with bioclim", total=None)
+        occurrences = BioclimService().augment_data_with_bioclimate(occurrences)
+        progress.remove_task(task_id)
 
-    fw = FileWriter()
+        if output == "output":
+            output = specie.replace(" ", "_")
 
-    typer.echo(f"Finished loading specie {specie} in {time.time() - now} seconds")
+        fw = FileWriter()
 
-    fw.write(occurrences, output_format, output)
+        typer.echo(f"Finished loading specie {specie} in {time.time() - now} seconds")
 
-    if save_map:
-        mapa = MapBuilder().build(occurrences)
-        mapa.save(f"data/{output}_ocurrences_map.html")
+        fw.write(occurrences, output_format, output)
 
-    # typer.echo(occurrences)
+        if save_map:
+            mapa = MapBuilder().build(occurrences)
+            mapa.save(f"data/{output}_ocurrences_map.html")
+
+        # typer.echo(occurrences)
 
 
 if __name__ == "__main__":
