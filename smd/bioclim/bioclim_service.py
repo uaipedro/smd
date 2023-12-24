b354df3528cdd1879eb5a436e1430dd690d240d2
uaipedro <pedromambelli@gmail.com>
2023-12-24 10:54:08 -0300

    Remove bioclim and excel files, add openpyxl and folium dependencies, and update load_specie.py

--- smd/bioclim/bioclim_service.py
+++ smd/bioclim/bioclim_service.py
@@ -1,4 +1,5 @@
 import fiona.transform
+import pandas as pd
 import rasterio.sample
 
 BIOCLIM_LAYERS = [
@@ -25,36 +26,31 @@ BIOCLIM_LAYERS = [
 
 
 class BioclimService:
-    def reproject_coords(self, src_crs, dst_crs, coords):
+    def reproject_coords(self, dataset, coords):
+        src_crs = "EPSG:4326"
+        dst_crs = dataset.crs.to_epsg()
         xs = [c[0] for c in coords]
         ys = [c[1] for c in coords]
         xs, ys = fiona.transform.transform(src_crs, dst_crs, xs, ys)
         return [[x, y] for x, y in zip(xs, ys)]
 
     def layer_path(self, layer):
-        return f"../data/bioclim/bioclim_{layer}.tif"
+        return f"bioclim/data/wc2.1_5m_bio_{layer+1}.tif"
 
-    def load_bioclimate_values(self, coords=[]):
-        results = [{"lon": coord[0], "lat": coord[1]} for coord in coords]
+    def augment_data_with_bioclimate(self, dataframe: pd.DataFrame):
+        dataframe.rename(
+            columns={"decimalLongitude": "lon", "decimalLatitude": "lat"}, inplace=True
+        )
 
-        for index, (alias, name) in enumerate(BIOCLIM_LAYERS):
-            with rasterio.open(self.layer_path(index)) as dataset:
-                src_crs = "EPSG:4326"
-                dst_crs = dataset.crs.to_epsg()
-                # dst_crs = dataset.crs.to_wkt()  # 'PROJCS["World_Mollweide",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS    84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mollweide"],PARAMETER["central_meridian",0],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH]]'
+        coords = list(zip(dataframe.lon, dataframe.lat))
 
-                # [longitude, latitude] not [lat, lon]...
-                new_coords = self.reproject_coords(src_crs, dst_crs, coords)
+        for index, (alias, _) in enumerate(BIOCLIM_LAYERS):
+            with rasterio.open(self.layer_path(index)) as raster:
+                reprojected_coords = self.reproject_coords(raster, coords)
 
-                values = list(rasterio.sample.sample_gen(dataset, new_coords))
+                values = list(rasterio.sample.sample_gen(raster, reprojected_coords))
 
                 for idx, ((lon, lat), value) in enumerate(zip(coords, values)):
-                    print(lon, lat, value[0])  # value[0] == band 1 value at lon, lat
-                    results[idx - 1][f"{alias}"] = value[0]
+                    dataframe.loc[idx, alias] = value[0]
 
-        self.bioclim_data = results
-
-    def get_bioclimate_values(self):
-        if not hasattr(self, "bioclim_data"):
-            self.load_bioclimate_values()
-        return self.bioclim_data
+        return dataframe
--- smd/load_specie.py
+++ smd/load_specie.py
@@ -1,9 +1,12 @@
-from pprint import pprint
+import time
 
 import typer
 from typing_extensions import Annotated
 
+from smd.bioclim.bioclim_service import BioclimService
 from smd.gbif.gbif_service import busca_especie_no_gbif
+from smd.view.file_writer import FileWriter
+from smd.view.map_builder import MapBuilder
 
 app = typer.Typer()
 
@@ -14,13 +17,30 @@ app = typer.Typer()
 @app.command()
 def load_specie(
     specie: Annotated[str, typer.Argument()],
-    output: Annotated[str, typer.Option(prompt=True)],
+    output: Annotated[str, typer.Option()] = "output",
+    output_format: Annotated[str, typer.Option()] = "xlsx",
+    save_map: Annotated[bool, typer.Option()] = False,
 ):
     typer.echo(f"Loading specie from gbif: {specie}")
+    now = time.time()
     occurrences = busca_especie_no_gbif(specie)
 
-    occurrences.to_csv(output, index=False)
-    pprint(occurrences)
+    occurrences = BioclimService().augment_data_with_bioclimate(occurrences)
+
+    if output == "output":
+        output = specie.replace(" ", "_")
+
+    fw = FileWriter()
+
+    typer.echo(f"Finished loading specie {specie} in {time.time() - now} seconds")
+
+    fw.write(occurrences, output_format, output)
+
+    if save_map:
+        mapa = MapBuilder().build(occurrences)
+        mapa.save(f"data/{output}_ocurrences_map.html")
+
+    # typer.echo(occurrences)
 
 
 if __name__ == "__main__":
