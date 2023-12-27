8a58bf0656e025472f8c3445bcd06b8372e89110
uaipedro <pedromambelli@gmail.com>
2023-12-26 22:55:16 -0300

    Add concurrent.futures import and implement parallel search in busca_especie_no_gbif function

--- src/gbif/gbif_service.py
+++ src/gbif/gbif_service.py
@@ -1,3 +1,5 @@
+import concurrent.futures
+
 import pandas as pd
 from pygbif import occurrences as occ
 
@@ -10,26 +12,29 @@ def busca_especie_no_gbif(nome_cientifico, country=None, size=100):
         "decimalLatitude",
         "decimalLongitude",
         "country",
-        # "coordinateUncertaintyInMeters",
         "year",
-        # "month",
-        # "day",
     ]
 
     complete = []
-    page = 0
-    while True:
+    pages = 10
+    limit = 300
+
+    def search_page(offset):
         results = occ.search(
             scientificName=nome_cientifico,
-            limit=300,
-            offset=300 * page,
+            limit=limit,
+            offset=offset,
             hasCoordinate=True,
             country=country,
         ).get("results")
-        complete = [*complete, *results]
-        if len(results) < 300:
-            break
-        page += 1
+        return results
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Mapeie a função de busca para todas as páginas em paralelo
+        all_results = executor.map(search_page, range(0, limit * pages, limit))
+
+    # Combine os resultados de todas as páginas
+    complete = [result for results in all_results for result in results]
 
     df = pd.DataFrame(complete)
 
